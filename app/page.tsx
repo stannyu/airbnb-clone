222a1a1a1ffdeebfa83f4fefde1cac3e8c7c99b7
stannyu <stas.yudzitski@yandex.ru>
2023-04-22 19:43:25 +0200

    create listing functionality added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 
-const inter = Inter({ subsets: ['latin'] })
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
-    <div className='text-rose-500 text-2xl'>Hi Airbnb!</div>  )
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard 
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
